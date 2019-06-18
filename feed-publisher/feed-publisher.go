598334c040e32faf64a1a0e4e23bc01ae08f9b83
Mylnev <Dmitry.Mylnev@Markets.com>
2019-06-18 21:32:41 +0300

    added logger

--- feed-publisher/feed-publisher.go
+++ feed-publisher/feed-publisher.go
@@ -1,7 +1,36 @@
 package main
 
+import (
+	log "github.com/sirupsen/logrus"
+	"os"
+	"time"
+)
+
+func init() {
+
+	log.SetFormatter(&log.TextFormatter{
+		DisableColors: true,
+		FullTimestamp: true,
+	})
+
+	log.SetOutput(os.Stdout)
+
+	log.SetLevel(log.TraceLevel)
+}
+
 func main() {
 
-	println("Hello world !!!")
+	for {
+		log.Debug("Hello world !!!")
+
+		contextLogger := log.WithFields(log.Fields{
+			"common": "this is a common field",
+			"other":  "I also should be logged always",
+		})
+
+		contextLogger.Info("I'll be logged with common and other field")
+
+		time.Sleep(1 * time.Second)
+	}
 
 }
