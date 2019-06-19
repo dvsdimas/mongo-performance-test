279fd0fae91eba63837b23d0e0937751dde5fdeb
Mylnev <Dmitry.Mylnev@Markets.com>
2019-06-19 20:56:49 +0300

    added properties

--- feed-publisher/feed-publisher.go
+++ feed-publisher/feed-publisher.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	//"fmt"
 	log "github.com/sirupsen/logrus"
 	"os"
-	"time"
+	//"time"
+	prop "github.com/magiconair/properties"
 )
+
+const propFileName string = "feed-publisher.properties"
+
+var defaultProperties = map[string]string{"key1": "value1",
+	"key2": "value2"}
 
 func init() {
 
@@ -18,19 +25,45 @@ func init() {
 	log.SetLevel(log.TraceLevel)
 }
 
+func loadProperties(path string, defaultProp *prop.Properties) *prop.Properties {
+
+	properties, err := prop.LoadFile(path, prop.UTF8)
+
+	if err != nil {
+
+		log.Warn("Cannot find properties file [" + path + "]. Will use default configuration")
+
+		properties = defaultProp
+	}
+
+	log.Info("Will use these properties:")
+
+	for k, v := range properties.Map() {
+		log.Info("key[" + k + "] value[" + v + "]")
+	}
+
+	return properties
+}
+
+// msq.ai
+
 func main() {
 
-	for {
-		log.Debug("Hello world !!!")
+	log.Info("feed-publisher is starting") // msq.ai
 
-		contextLogger := log.WithFields(log.Fields{
-			"common": "this is a common field",
-			"other":  "I also should be logged always",
-		})
+	properties := loadProperties(propFileName, prop.LoadMap(defaultProperties))
 
-		contextLogger.Info("I'll be logged with common and other field")
-
-		time.Sleep(1 * time.Second)
+	if properties == nil {
+		log.Fatal("Properties has been set !!!")
 	}
 
 }
+
+//contextLogger := log.WithFields(log.Fields{
+//	"common": "this is a common field",
+//	"other":  "I also should be logged always",
+//})
+//
+//contextLogger.Info("I'll be logged with common and other field")
+//
+//time.Sleep(1 * time.Second)
